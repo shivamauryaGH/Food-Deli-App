11d9fe5d09fe5637f6d30c90c56e99379b270f20
shivamauryaGH <521241@student.nitandhra.ac.in>
2024-07-17 06:29:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {lazy,Suspense} from "react";
+import './index.css';
+import Header  from "./components/Header";
+import Body from "./components/Body";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"; import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+import Shimmer from "./components/Shimmer";
+//import Grocery from "./components/Grocery";
+import {Provider} from "react-redux";
+import appStore from "./utils/appStore";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import Collection from "./components/Collection";
+import Search from "./components/Search"
+
+
+const Grocery = lazy(()=> import("./components/Grocery"));
+
+
+const App = () => {
+  return (
+    <div style={{backgroundColor: "white"}}>
+    <Provider store={appStore}>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Body />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/grocery" element={<Suspense fallback={<Shimmer/>}><Grocery /></Suspense>} />
+    <Route path="*" element={<Error />} />
+    <Route path="/restaurants/:resId" element={<RestaurantMenu />} />
+    <Route path="/collection/:colId" element={<Collection />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/search" element={<Search />} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    </Provider>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+const About = () => {
+return (
+    <>
+<div className="about">
+    <div className="about-head"><h1>About Me</h1></div>
+    <div className="about2">
+    
+    <div className="about3">
+        
+        <p>I'm Shivam Maurya, a final year student pursuing BTech in Electrical and Electronics Engineering from NIT Andhra Pradesh. I am an active learner who believes in the value of technology and teamwork. Skilled in C++, Data Structures , Algorithms , I am an aspiring Web Developer. Looking forward to great opportunities to learn new technologies , skills and willing to work under pressure on projects and deadline with the given time limit or target.</p>
+        <a href="https://www.linkedin.com/in/shivam-maurya-910a40271" target="blank"><button className="main-btn">Linkdin</button></a>
+    </div>
+    </div>
+</div>
+
+ 
+ </>
+)}
+
+export default About;
